a2b29d791e5ada1f472de358160611e8d74a24ef
josealcivar <alcivar372@gmail.com>
2022-10-29 07:34:44 -0500

    estado query set

--- projects/api.py
+++ projects/api.py
@@ -1,9 +1,10 @@
 from projects.serializers import ProjectSerializer
 from .models import Project
 from rest_framework import viewsets, permissions
-
+import modelo_mixto
 
 class ProjectViewSet(viewsets.ModelViewSet):
+    print(modelo_mixto)
     queryset = Project.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = ProjectSerializer
