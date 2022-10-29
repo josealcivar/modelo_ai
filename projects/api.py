7408371c79b9bc413c233520ad1f68bd2c66f78d
josealcivar <alcivar372@gmail.com>
2022-10-29 01:01:44 -0500

    git commit init

---
+++ projects/api.py
@@ -0,0 +1,9 @@
+from projects.serializers import ProjectSerializer
+from .models import Project
+from rest_framework import viewsets, permissions
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProjectSerializer
---
+++ projects/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Project
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Project
+        fields = ('id','title','description','technology', 'create_at')
+        # fields='__all__' 
+        read_only_fields = ('created_at', )
+        
+
+  
