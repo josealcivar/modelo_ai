30091c58e0d6084e482f5012f100b7a6a0998bc6
josealcivar <alcivar372@gmail.com>
2022-10-30 14:43:08 -0500

    ruta del modelo

--- projects/urls.py
+++ projects/urls.py
@@ -7,6 +7,8 @@ router = routers.DefaultRouter()
 
 router.register('api/projects', ProjectViewSet, 'projects')
 
+router.register('api/projects/modelos', ProjectViewSet.list, 'modelos')
+
 urlpatterns = router.urls
 
 
