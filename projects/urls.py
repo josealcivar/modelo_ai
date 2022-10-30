c4846b6299106a1dd00f99af0f76cd7b9170eab8
josealcivar <alcivar372@gmail.com>
2022-10-30 18:04:32 -0500

    funcion

--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,11 @@
 from rest_framework import routers
 from django.urls import path
 from .api import ProjectViewSet
-
+from projects import views
 
 router = routers.DefaultRouter()
 
-router.register('api/projects', ProjectViewSet, 'projects') 
+router.register('api/projects/modelo', views, 'projects') 
 
 # router.register('api/projects/modelos', ProjectViewSet.as_view({
 #     'get':'getModelo'
