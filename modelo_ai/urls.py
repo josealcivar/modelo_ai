372082d2f3edee53349889a02f7f1bc9f5478e83
josealcivar <alcivar372@gmail.com>
2022-10-30 20:31:01 -0500

    views projects

--- modelo_ai/urls.py
+++ modelo_ai/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from projects.views import views
+from projects import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
